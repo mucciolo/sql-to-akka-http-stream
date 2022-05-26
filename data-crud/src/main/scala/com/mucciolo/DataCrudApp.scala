3bdb81b3eec0c70412401811cca8cf3a0ac3bde9
mucciolo <diego_mucciolo@hotmail.com>
2022-05-26 10:33:05 -0300

    renamed HttpServerApp to DataCrudApp

--- data-crud/src/main/scala/com/mucciolo/DataCrudApp.scala
+++ data-crud/src/main/scala/com/mucciolo/DataCrudApp.scala
@@ -3,6 +3,6 @@ package com.mucciolo
 import cats.effect.{IO, IOApp}
 import com.mucciolo.server.HttpServer
 
-object HttpServerApp extends IOApp.Simple {
+object DataCrudApp extends IOApp.Simple {
   def run: IO[Unit] = HttpServer.run()
 }
